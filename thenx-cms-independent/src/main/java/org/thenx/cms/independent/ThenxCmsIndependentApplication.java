e77c8ff7d9c7e913ecbfb272fb72914b8094e126
thenx-wei <thenx-wei@thenx.org>
2020-01-12 20:44:37 +0800

    add: 构建单体架构版服务，包括 Spring Boot、Spring Security、Redis、MySQL

---
+++ thenx-cms-independent/src/main/java/org/thenx/cms/independent/ThenxCmsIndependentApplication.java
@@ -0,0 +1,13 @@
+package org.thenx.cms.independent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThenxCmsIndependentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThenxCmsIndependentApplication.class, args);
+    }
+
+}
---
+++ thenx-cms-independent/src/test/java/org/thenx/cms/independent/ThenxCmsIndependentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thenx.cms.independent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThenxCmsIndependentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
